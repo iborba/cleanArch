886e1224a8185b9a548ecd09331112f0f54be301
iborba <israel.borba@gmail.com>
2024-01-16 14:50:51 -0300

    start

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
 import { AdvisorController } from './src/_external/controllers/advisor-controller';
 
-(async () => {
-  console.log('start');
-  const controller = new AdvisorController();
-  const response = await controller.getAdvisorByEmail('email');
+async function main() {
+  try {
+    console.log('start');
+    const controller = new AdvisorController();
+    const response = await controller.getAdvisorByEmail('email');
+    
+    console.log(response);
+  } catch (error) {
+    console.log(error)
+  }
+}
 
-  console.log(response);
-})();
+main().then()
